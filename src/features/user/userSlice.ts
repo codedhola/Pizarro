bdf08fb6a64e045635ae3ed7083fd0af1e1a43d0
codedhola <codedhola2000@gmail.com>
2024-02-23 00:47:03 +0100

    configured redux

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -1,4 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
 import { getAddress } from "../../services/apiGeocoding";
+
+const initialState = {
+  username: ""
+}
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    updateName(state, action){
+      state.username = action.payload
+    }
+  }
+})
 
 function getPosition() {
   return new Promise(function (resolve, reject) {
@@ -21,3 +36,8 @@ async function fetchAddress() {
   // 3) Then we return an object with the data that we are interested in
   return { position, address };
 }
+
+
+export const { updateName } = userSlice.actions
+
+export default userSlice.reducer
